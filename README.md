e8ad6d492fe121d3037c7c876e654eada46e4c62
Abhishek Jha <iamabhishek1310@gmail.com>
2026-04-04 00:44:21 +0530

    Init: project initialized with tailwind, react and bun

---
+++ README.md
@@ -0,0 +1,15 @@
+# Findash | A Finance Dashboard | Know your finance better
+
+
+# Deployed URL 
+
+https://findash-icoderabhishek.vercel.app/
+
+# Installation
+
+```
+git clone https://github.com/iCoderabhishek/findash.git
+
+cd findash && bun i && bun dev
+
+```
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-2xl text-center text-white translate-y-1/2 font-mono mt-10'>Hi there, this is me <span className='text-red-500 underline decoration-red-500'>Abhishek</span>, Starting this project!</h1>
+    </>
+  )
+}
+
+export default App
